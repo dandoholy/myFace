a81feadf50ab7ae392bd97eac2f9faeb68b5233d
Dan <dandily@bu.edu>
2018-03-06 18:04:24 -0500

    start work on birthday input

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import LoginFormContainer from './session_forms/login_form_container';
+import SignupFormContainer from './session_forms/signup_form_container';
 
 const App = () => {
   return (
     <div>
       <h1>Your Face Here</h1>
       <LoginFormContainer />
+      <SignupFormContainer />
+
     </div>
   );
 }
--- frontend/components/session_forms/signup_form.jsx
+++ frontend/components/session_forms/signup_form.jsx
@@ -11,6 +11,8 @@ class SignupForm extends React.Component {
       birthday: '',
       gender: ''
     };
+    this.birthday = ['0000', '00', '00'];
+
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
@@ -20,6 +22,16 @@ class SignupForm extends React.Component {
     };
   }
 
+  updateBirthday(idx) {
+    return (e) => {
+      let res = e.currentTarget.value
+      if (res < 10) { res = `0${res}`}
+      this.birthday[idx] =res ;
+      const birthday = this.birthday.join('-');
+      this.setState({ birthday })
+    }
+  }
+
   handleSubmit () {
     e.preventDefault();
     const user = merge({}, this.state);
@@ -27,10 +39,55 @@ class SignupForm extends React.Component {
   }
 
   render() {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const monthOpts = months.map((mo, idx) => {
+      return ( <option key={idx} value={idx+1}>{mo}</option>);
+    })
     return(
       <div className='signup-form-container'>
-        
+        <div className='signup-header-box'>
+          <div className='signup-header-line1'>Create a New Account</div>
+          <div className='signup-header-line2'>It’s free and always will be.</div>
+        </div>
         <form onSubmit={this.handleSubmit} className='signup-form-box'>
+          <input type='text'
+            value={this.state.first_name}
+            onChange={this.update('first_name')}
+            className='signup-name-input'
+            placeholder='First name'
+          />
+          <input type='text'
+            value={this.state.last_name}
+            onChange={this.update('last_name')}
+            className='signup-name-input'
+            placeholder='Last name'
+          />
+          <input type='text'
+            value={this.state.email}
+            onChange={this.update('email')}
+            className='signup-input'
+            placeholder='Email address'
+          />
+          <input type='password'
+            value={this.state.password}
+            onChange={this.update('password')}
+            className='signup-input'
+            placeholder='New password'
+          />
+          <label>Birthday
+            <select value={this.birthday[1]} onChange={this.updateBirthday(1)}>
+              {monthOpts}
+            </select>
+            <input type='date'
+              value={this.state.birthday}
+              onChange={this.update('birthday')}
+              className='signup-birthday-input'
+            />
+          </label>
+          <input className='signup-submit'
+            type='submit'
+            value={this.props.formType}
+          />
         </form>
       </div>
     );
